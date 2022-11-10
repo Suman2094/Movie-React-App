7ea801f547bee335878de0f5933c082b76a0a6c8
Suman2094 <sbaniya94@gmail.com>
2022-11-10 00:24:12 -0600

    started movie app by printing App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { useState }  from 'react';
-import './App.css';
+import React from "react";
 
+const App = () => {
 
-const App = () =>{
-  const [counter, setCounter] = useState(0);
-
-  return (
-    <div className="App">
-      <button onClick={()=> setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={()=> setCounter((prevCount) => prevCount + 1)}>+</button>      
-    </div>
-  );
+    return(
+        <h1>App is good</h1>
+    );
 }
 
 export default App;
- 
